f13a1fa3ed7afba037e3d236039d3606bad7dada
Vinicius <viniciusbarros@alunos.utfpr.edu.br>
2022-02-15 08:06:30 -0300

    update files

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,27 @@
 import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  const { data: session } = useSession();
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={(e) => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Vinícius Cerqueira
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={(e) => signIn('github')}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
