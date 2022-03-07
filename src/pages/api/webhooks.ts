1e230623895fcc6aacf7176c2e8fce4657c936c0
Vinícius Cerqueira Barros <viniciusbarros@alunos.utfpr.edu.br>
2022-03-07 12:54:33 -0300

    update files

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -37,11 +37,29 @@ const webhook = async (req: NextApiRequest, res: NextApiResponse) => {
 
     const { type } = event;
 
-    const relevantEvents = new Set(["checkout.session.completed"]);
+    const relevantEvents = new Set([
+      "checkout.session.completed",
+      "customer.subscription.created",
+      "customer.subscription.updated",
+      "customer.subscription.deleted",
+    ]);
 
     if (relevantEvents.has(type)) {
       try {
         switch (type) {
+          case "customer.subscription.created":
+
+          case "customer.subscription.updated":
+
+          case "customer.subscription.deleted":
+            const subscription = event.data.object as Stripe.Subscription;
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              type === "customer.subscription.created"
+            );
+            break;
           case "checkout.session.completed":
             const checkoutSession = event.data
               .object as Stripe.Checkout.Session;
