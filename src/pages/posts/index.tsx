bb041bb3687bbbdbd25f06c2c02877b7a1170861
Vinícius Cerqueira Barros <viniciusbarros@alunos.utfpr.edu.br>
2022-03-08 14:35:19 -0300

    update files

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,46 @@
+import Head from "next/head";
+import styles from "./styles.module.scss";
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>12 de Jun de 2018</time>
+            <strong>Integração do Material UI com ReactJS</strong>
+            <p>
+              Algo que acontece com muitos Devs é não ter alguma aptidão ou
+              mesmo não ter tempo hábil para fazer os Designs e Layouts de suas
+              aplicações, mas quem não gosta de um front-end bonitinho?
+            </p>
+          </a>
+
+          <a href="#">
+            <time>12 de Jun de 2018</time>
+            <strong>Integração do Material UI com ReactJS</strong>
+            <p>
+              Algo que acontece com muitos Devs é não ter alguma aptidão ou
+              mesmo não ter tempo hábil para fazer os Designs e Layouts de suas
+              aplicações, mas quem não gosta de um front-end bonitinho?
+            </p>
+          </a>
+
+          <a href="#">
+            <time>12 de Jun de 2018</time>
+            <strong>Integração do Material UI com ReactJS</strong>
+            <p>
+              Algo que acontece com muitos Devs é não ter alguma aptidão ou
+              mesmo não ter tempo hábil para fazer os Designs e Layouts de suas
+              aplicações, mas quem não gosta de um front-end bonitinho?
+            </p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
